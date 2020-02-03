61d0272348b0944f64b0efd473759819c19efc0f
rfcniblon <lambinet.nicolas@orange.fr>
2020-02-03 13:12:12 +0100

    de la mort

--- back/server.js
+++ back/server.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const parser = require("body-parser");
 const app = express();
+const contact = require("/routes/contact");
+const users = require("/routes/users");
+const panier = require("/routes/panier");
+const evenement = require("/routes/evenement");
 
 
 app.use(parser.json());
+app.use(cors());
+app.use("/images", express.static("public/images"));
+// app.use("/recrutement", express.static("public/recrutement"));
+app.use(parser.urlencoded({
+  extended: true
+}));
 
+app.use("/", users);
+app.use("/", contact);
+app.use("/", panier);
+app.use("/", evenement);
 // home page
 app.get(
     "/",
