0935eb826329d84f7b247734584e551c44ad5712
rfcniblon <lambinet.nicolas@orange.fr>
2020-02-03 14:20:29 +0100

    upload de ces mords

---
+++ front/src/component/Accueil/Accueil.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class Accueil extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+             <div>
+            <p>azertyu</p>
+            </div>
+         );
+    }
+}
+ 
+export default Accueil;
---
+++ front/src/component/admin/Adminpage.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+class Adminpage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <div>
+            
+        </div> );
+    }
+}
+ 
+export default Adminpage;
